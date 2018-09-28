73bd6d498e9446050d1ad15f5c31fc84a6afd330
Stavista <mit13020@byui.edu>
2018-09-27 22:35:34 -0600

    Completed Step 5

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -35,6 +35,38 @@ namespace Math_Quiz
             InitializeComponent();
         }
 
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+                return true;
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft--;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                startButton.Enabled = true;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
 
@@ -51,20 +83,14 @@ namespace Math_Quiz
             startButton.Enabled = false;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void answer_Enter(object sender, EventArgs e)
         {
-            if (timeLeft > 0)
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
             {
-                timeLeft = timeLeft - 1;
-                timeLabel.Text = timeLeft + " seconds";
-            }
-            else
-            {
-                timer1.Stop();
-                timeLabel.Text = "Time's up!";
-                MessageBox.Show("You didn't finish in time.", "Sorry!");
-                sum.Value = addend1 + addend2;
-                startButton.Enabled = true;
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
             }
         }
     }
